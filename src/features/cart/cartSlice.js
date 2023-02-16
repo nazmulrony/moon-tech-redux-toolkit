63a55757e905141100acd99335c81e17fafa0c0a
nazmulrony <nhrony65@gmail.com>
2023-02-16 16:33:18 +0600

    filtering added

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,47 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initialState = {
+    cart: [],
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const selectedProduct = state.cart.find(
+                (product) => product._id === action.payload._id
+            );
+            if (!selectedProduct) {
+                state.cart.push({ ...action.payload, quantity: 1 });
+            } else {
+                selectedProduct.quantity += 1;
+                // state.cart = state.cart
+                //     .filter((product) => product._id !== selectedProduct._id)
+                //     .push(selectedProduct);
+            }
+        },
+
+        //
+        //
+        //
+        //
+        removeFromCart: (state, action) => {
+            const selectedProduct = state.cart.find(
+                (product) => product._id === action.payload._id
+            );
+            console.log(selectedProduct.quantity);
+            if (selectedProduct.quantity > 1) {
+                selectedProduct.quantity -= 1;
+            } else {
+                state.cart = state.cart.filter(
+                    (product) => product._id !== selectedProduct._id
+                );
+            }
+        },
+    },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
